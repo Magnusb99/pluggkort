28759ea336fc2a6f37643019924234ecb3f0c055
Magnus Bengtsson <magnusb99@hotmail.se>
2025-08-17 22:34:59 +0200

    krunegard

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   const [kursKoder, setkursKoder] = useState([]);
   const [valdKurs, setValdKurs] = useState([]);
-
+  const [showMarkus, setMarkus] = useState(true);
   useEffect(() => {
     async function fetchKurser() {
       try {
@@ -29,17 +29,74 @@ function App() {
 
     fetchKurser();
   }, []);
+
+  function ShowMarkus() {
+    setMarkus(!showMarkus);
+  }
+
   return (
     <Router>
       <header className="NAVBAR">
         <h1>Ezplugg</h1>
+        <div className="mp3Player">
+          {showMarkus && (
+            <div className="imgWrap">
+              <img src="./src/assets/markus/krunegard.jpg" />
+            </div>
+          )}
+          <audio controls>
+            <source
+              src="./src/assets/markus/krunegard.mp3"
+              type="audio/mp3"
+            ></source>
+          </audio>
+          <button className="" onClick={ShowMarkus}>
+            {showMarkus ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#000000"
+              >
+                <path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#000000"
+              >
+                <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+              </svg>
+            )}
+          </button>
+        </div>
         <nav>
-          <Link className="btn" to="/">
-            Hem
+          <Link className="btn linkBtn" to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+            </svg>
           </Link>
 
-          <Link className="btn" to="/add">
-            Lägg in frågor
+          <Link className="btn linkBtn" to="/add">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z" />
+            </svg>
           </Link>
         </nav>
       </header>
@@ -62,8 +119,8 @@ function App() {
                     >
                       <path d="M420-680v-120h120v120H420Zm0 520v-120h120v120H420Z" />
                     </svg>
-                    Laddar
-                  </p> // eller null
+                    <p className="loadingText">Laddar</p>
+                  </p>
                 )}
                 {kursKoder.length > 0 && (
                   <CourseChooser
